6b63e376a95404704a24614c475a4ae0f04b88c1
Talish Kumar <101531410+TalishKumar@users.noreply.github.com>
2022-06-05 16:00:17 -0500

    Update EmailAddressValidation.cpp

--- EmailAddressValidation.cpp
+++ EmailAddressValidation.cpp
@@ -1,101 +1,71 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
+
 
 int main()   //DO NOT change the 'main' signature
 {
-    //Get user SSN
-    cout << "Enter the SSN number:\n";
-    string userSSN{};
-    cin >> userSSN;
+    //Get use email ID
+    string userEmail{};
+    getline(cin, userEmail);
     
-    //Get user SSN length
-    int userSSNLen{};
-    for (int i{0}; i < userSSN[i] != '\0'; ++i)
+    //Get length of email ID
+    int userEmailLen{0};
+    for (int i {0}; i < userEmail[i] != '\0'; ++i)
     {
-        ++userSSNLen;
+        ++userEmailLen;
     }
     
-    //Create array for chars
-    char userSSNCh[10]{};
+    //Input validate email (whitespace)
+    bool validEmailSpace{false};
     
-    //Create array for digits
-    int userSSNInt[10]{};
-    
-    //Iterators
-    int iteratorCh{0};
-    int iteratorInt{0};
-    
-    //Put char & int types in its own array
-    for (int i{0}; i < userSSNLen; ++i)
+    for (int i{0}; i < userEmailLen; ++i)
     {
-        if(isalpha(userSSN[i]))
+        if (userEmail[i] - 1 == ' ')
         {
-           userSSNCh[iteratorCh] = userSSN[i];
-           
-           ++iteratorCh;
+            break;
         }
-        else if(isdigit(userSSN[i]))
+        else
         {
-            userSSNInt[iteratorInt] = userSSN[i] - 48;
+            validEmailSpace = true;
+        }
+    }
+    
+    //Input validate email (has @ & .)
+    bool validEmailSymbol1{false};
+    bool validEmailSymbol2{false};
+    bool hasValidSymbols{false};
+    
+    for (int i {0}; i < userEmailLen; ++i)
+    {
+        if (userEmail[i] == 64)
+        {
+            validEmailSymbol1 = true;
             
-            ++iteratorInt;
+            if (validEmailSymbol2)
+            {
+                validEmailSymbol2 = false;
+                break;
+            }
+        }
+        if (userEmail[i] == 46)
+        {
+            validEmailSymbol2 = true;
         }
     }
     
-    //Length of char & int array
-    int userSSNChLen{sizeof(userSSNCh)/sizeof(userSSNCh[0])};
-    int userSSNIntLen{sizeof(userSSNInt)/sizeof(userSSNInt[0])};
-    
-    //Input validate chars
-    bool validCh{false};
-    
-    for (int i{0}; i < userSSNChLen; ++i)
+    if (validEmailSymbol1 && validEmailSymbol2)
     {
-        if (userSSNCh[i] < 91 && userSSNCh[i] > 64)
-        {
-            validCh = true;
-        }
-        else if (userSSNCh[i] == 0)
-        {
-            validCh = true;
-        }
-        else
-        {
-            validCh = false;
-            break;
-        }
+        hasValidSymbols = true;
     }
     
-    //Input validate int
-    bool validInt{false};
-    
-    for (int i{0}; i < userSSNIntLen; ++i)
+    //Output conditional statement
+    if (validEmailSpace && hasValidSymbols)
     {
-        if (userSSNInt[i] < 0 || userSSNInt[i] > 9)
-        {
-            validInt = false;
-            break;
-        }
-        else
-        {
-            validInt = true;
-        }
-    }
-    
-    //Output condition statement
-    if (validInt && validCh)
-    {
-        cout << "Valid SSN number";
-    }
-    else if (userSSNLen != 10)
-    {
-        cout << "Invalid SSN number";
-        return 0;
+        cout << "Valid email Id";
     }
     else
     {
-        cout << "Invalid SSN number";
+        cout << "Invalid email Id";
     }
     
     return 0;
