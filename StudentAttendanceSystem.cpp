a0896b702090d0d1e34ad1333e30e386a34e1f4b
Talish Kumar <101531410+TalishKumar@users.noreply.github.com>
2022-06-05 15:53:54 -0500

    Create StudentAttendanceSystem.cpp
    A simple abstract representation of the tasks that an attendance management system would perform.

---
+++ StudentAttendanceSystem.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+
+int main()   //DO NOT change the 'main' signature
+{
+    //Get number of students
+    cout << "Enter number of students\n";
+    short int numOfStudents{};
+    cin >> numOfStudents;
+    
+    cout << '\n';
+    
+    //Initialize array for student information
+    string student[numOfStudents][3]{};
+    
+    //For each student, get their information
+    for (int i{0}; i < numOfStudents; ++i)
+    {
+        cout << "Enter roll number: ";
+        cin >> student[i][0];
+        
+        cout << "Enter name: ";
+        cin >> student[i][1];
+        
+        cout << "Enter gender: ";
+        cin >> student[i][2];
+        
+        cout << '\n';
+    }
+    
+    //Get roll number of student that needs to be found
+    cout << "Enter the roll number of student to view: ";
+    string studentRollNum{};
+    cin >> studentRollNum;
+    
+    //Find the student with roll number 
+    for (int i{0}; i < numOfStudents; ++i)
+    {
+        if (studentRollNum == student[i][0])
+        {
+            cout << student[i][1] << '\n';
+            cout << student[i][2];
+        }
+    }
+    
+    return 0;
+}
